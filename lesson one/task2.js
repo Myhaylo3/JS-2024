4c8e994dd89042034077c94c69fabf2d7bc099f8
Myhaylo3 <myhailokiselyk200214@gmail.com>
2024-05-01 17:31:23 +0200

    task lesson one

---
+++ lesson one/task2.js
@@ -0,0 +1,7 @@
+let n = firstName = `Kiselyk`;
+let o = lastName = `Mykhailo`;
+let p = middleName = `Oleksiiovych`;
+
+console.log(n)
+console.log(o)
+console.log(p)
---
+++ lesson one/task3.js
@@ -0,0 +1,7 @@
+let q = 100;
+let r = '100';
+let s = true;
+
+console.log(typeof q);
+console.log(typeof r);
+console.log(typeof s);
